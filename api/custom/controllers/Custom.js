20feb76248a8af0b974c1b98039e8c89d5b6a1bb
jukialen <57296419+jukialen@users.noreply.github.com>
2021-11-29 18:10:56 +0100

    Adding CORS support | Update: logout function and Documentation

--- api/custom/controllers/Custom.js
+++ api/custom/controllers/Custom.js
@@ -1,10 +1,24 @@
 "use strict";
+
 module.exports = {
   async logout(ctx) {
-    ctx.cookies.set("user", null);
-    ctx.send({
-      authorized: true,
-      message: "Successfully destroyed session",
-    });
-  },
+    const token = ctx.cookies.get('user');
+    const tokenSig = ctx.cookies.get('user.sig');
+
+    if (token != undefined) {
+      ctx.cookies.set('user', null);
+      ctx.cookies.set('user.sig', null);
+
+      ctx.send({
+        authorized: true,
+        message: "Successfully logged out.",
+      });
+    } else {
+      ctx.send({
+        authorized: false,
+        message: "Unable to logout. You are not logged in.",
+      });
+    }
+
+  }
 };
