703b205eb95f385497d636f70b35f2c8abd652a1
jukialen <57296419+jukialen@users.noreply.github.com>
2021-12-04 17:26:35 +0100

    2 attempt to fix error 500 in /auth/local/ on (only) production (2nd series)

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6a1a0de1abbca755fbdf7501ac3976f4'),
