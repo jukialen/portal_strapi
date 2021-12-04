f8cfa4f5697b727388b433494a446227b8eb811e
jukialen <57296419+jukialen@users.noreply.github.com>
2021-12-04 17:34:30 +0100

    3 attempt

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6a1a0de1abbca755fbdf7501ac3976f4'),
